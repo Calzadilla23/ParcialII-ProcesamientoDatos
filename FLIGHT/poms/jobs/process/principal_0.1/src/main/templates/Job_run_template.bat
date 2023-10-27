5bc26b1997e60154a9f7214b41ce9da902e77f0f
Calzadilla23 <cr15089@ues.edu.sv>
2023-10-27 17:42:13 -0600

    talendProject
    Proyecto Talend
    Procesado y llenado de datos Talend

---
+++ FLIGHT/poms/jobs/process/principal_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms2048M -Xmx 4096M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.13.2.jar;../lib/log4j-api-2.13.2.jar;../lib/log4j-core-2.13.2.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/mysql-connector-java-8.0.18.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/crypto-utils-0.31.12.jar;principal_0_1.jar;dimairline_0_1.jar;dimcity_0_1.jar;dimairport_0_1.jar; flight.principal_0_1.Principal %*
